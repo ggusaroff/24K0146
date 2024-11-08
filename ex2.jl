871c8b2986fc2d115e374955aae21ab04d127cf5
Gleb Gusarov <gusarov.gleb.crimea@gmail.com>
2024-11-08 11:42:47 +0300

    ex2.jl

--- ex2.jl
+++ ex2.jl
@@ -1,5 +1,5 @@
 using HorizonSideRobots
-
+robot = Robot(animate=true,8,8)
 function do_upora!(robot,side)
     count=0
     while !isborder(robot,side)
@@ -9,20 +9,24 @@ function do_upora!(robot,side)
     return count
 end
 
-function HorizonSideRobots.move!(robot,side,num_steps:Integer)
-    for i in 1:num_steps
-        move!(robot,side)
-    end
-end
-
-function perimetr(robot)
+function go(robot)
     num_steps_sud  = do_upora!(robot,Sud)
     num_steps_west  = do_upora!(robot,West)
-    for side ∈ (Nord,Ost,Sud,West)
-        while !isborder(robot,side)
+    while !isborder(robot,Ost)
+        while !isborder(robot,Nord)
             putmarker!(robot)
-            move!(robot,side)
+            move!(robot,Nord)
+            putmarker!(robot)
         end
+        move!(robot,Ost)
+        while !isborder(robot,Sud)
+            putmarker!(robot)
+            move!(robot,Sud)
+            putmarker!(robot)
+        end
+    end
+    while !isborder(robot,West)
+        move!(robot,West)
     end
     move!(robot, Nord, num_steps_sud)
     move!(robot,Ost,num_steps_west)
